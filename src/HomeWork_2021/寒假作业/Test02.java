66ee8801cab6986e1a20ee1296fce4325e4365e8
Class-util <wb1945621909@aliyun.com>
2021-01-20 22:09:21 +0800

    2021.1.20 : 寒假作业

---
+++ src/HomeWork_2021/寒假作业/Test02.java
@@ -0,0 +1,19 @@
+package HomeWork_2021.寒假作业;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:吴博
+ * Date:2021 01 20
+ * Time:17:47
+ */
+public class Test02 {
+    public static void main(String args[]) {
+        int i = 7;
+        do {
+            System.out.println(--i);
+            --i;
+        } while (i != 0);
+        System.out.println(i);
+    }
+}
