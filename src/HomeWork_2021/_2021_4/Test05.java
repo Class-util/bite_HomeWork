4f5ecbb355657a3cfdfe72fb2e7de583efff4f59
Class-util <wb1945621909@aliyun.com>
2021-04-19 19:32:28 +0800

    2021.4.19：新web阶段考试

--- src/HomeWork_2021/_2021_4/Test05.java
+++ src/HomeWork_2021/_2021_4/Test05.java
@@ -11,8 +11,50 @@ import java.util.*;
  */
 public class Test05 {
     public static void main(String[] args) {
-        func1();
+        func2();
     }
+
+
+    public static void myFun(){
+        Scanner sc = new Scanner(System.in);
+        //n个数字
+        int n = sc.nextInt();
+        //使用arrayList存储数字，一个为数字集，一个为计数集
+        List<Integer> list = new ArrayList();
+        List<Integer> ret = new ArrayList();
+        //存储数字
+        for(int i = 0; i < n; i++){
+            list.add(sc.nextInt());
+        }
+        //从小到大进行排序
+        list.sort((o1,o2) -> o1-o2);
+        int j = 0;
+        //记录数字出现几次
+        int flog = 0;
+        int num = list.get(0);
+        while(j < n){
+            //开始进行比较
+            //因为排完序，所以直接比较，相邻有相同的就flog++
+            while(j < n && num == list.get(j)){
+                flog++;
+                j++;
+            }
+            //执行至此，说明已经将该数字出现的次数记录完，判断是奇数还是偶数
+            if(flog % 2 == 1){
+                //为奇数，则添加进行ret集合，注意是在j-1和j不相同循环才结束的，我们要添加的是j-1位置的元素，j还没有开始自己的计数
+                ret.add(list.get(j - 1));
+            }
+            //如果j<n说明数字还没比较晚
+            if(j < n){
+                //将j位置的数字记录下，开始计数循环
+                num = list.get(j);
+            }
+            flog = 0;
+        }
+        //明确只有两个这样的数字，直接定性输出即可
+        System.out.println(ret.get(0) + " " + ret.get(1));
+    }
+
 
     //数组中值出现了一次的数字
     public static void func2(){
--- src/HomeWork_2021/_2021_4/Test06.java
+++ src/HomeWork_2021/_2021_4/Test06.java
@@ -12,8 +12,72 @@ import java.util.Stack;
  */
 public class Test06 {
     public static void main(String[] args) {
-        func();
+        Scanner scanner = new Scanner(System.in);
+        int N =  scanner.nextInt();
+        int[] array  = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < N ; i++) {
+            toFind1(array, array[i], i);
+        }
     }
+
+    private static void toFind1(int[] array, int num, int i) {
+        int left = -1;
+        int right = -1;
+        for (int j = i; j >= 0 ; j--) {
+            if (array[j] < num) {
+                left = j;
+                break;
+            }
+        }
+        for (int j = i+1; j < array.length ; j++) {
+            if (array[j] < num) {
+                right = j;
+                break;
+            }
+        }
+        System.out.println(left + " " + right);
+    }
+
+    public static void main1(String[] args) {
+        func1();
+    }
+
+    public static void func1(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            toFind(arr,arr[i],i);
+        }
+    }
+
+    private static void toFind(int[] arr, int num, int i) {
+        //初始值都为-1，放便最右边和最左边的值，都会存在一个-1；
+        int left = -1;
+        int right = -1;
+        //找到左边比他小的,向左走
+        for (int j = i; j >= 0; j--) {
+            if(arr[j] < num){
+                left = j;
+                break;
+            }
+        }
+        //找到右边最小的值,向右走
+        for (int j = i + 1; j < arr.length; j++) {
+            if(arr[j] < num){
+                right = j;
+                break;
+            }
+        }
+        System.out.println(left + " " + right);
+    }
+
     //单调栈结构
     public static void func(){
         Scanner sc = new Scanner(System.in);
