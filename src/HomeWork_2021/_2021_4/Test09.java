20bf2c5aa0b87bcc0fe3c3f44f369737572596d7
Class-util <wb1945621909@aliyun.com>
2021-04-21 16:51:51 +0800

    2021.4.21：4.20的每日一题

---
+++ src/HomeWork_2021/_2021_4/Test09.java
@@ -0,0 +1,56 @@
+package HomeWork_2021._2021_4;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:吴博
+ * Date:2021 04 21
+ * Time:16:08
+ */
+
+public class Test09{
+
+    //数字颠倒
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder(n + "");
+        System.out.println(sb.reverse());
+    }
+
+    //末尾0的个数
+    public static void main2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = n; i >= 5; i--) {
+            int tmp = i;
+            while (tmp % 5 == 0){
+                count++;
+                tmp /= 5;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void main1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long num = 1;
+        for(int i = 1;i <= n; i++){
+            num*=i;
+        }
+        String str = num + "";
+        int count = 0;
+        for (int length = str.length() - 1; length >= 0; length--) {
+            if("0".equals(str.charAt(length) + "")){
+                count++;
+            }else {
+                break;
+            }
+        }
+        System.out.println(count);
+    }
+}
